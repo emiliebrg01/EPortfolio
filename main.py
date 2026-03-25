f6a1a8651719a02abab2a1f4c0f558163656b7ef
emiliebrg01 <emilie.bourg@epfedu.fr>
2026-03-25 12:52:52 +0100

    Avancement du fichier main

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ class Person:
         self.user_age = age
         self.statut = status
 
-
     def create_person():
         return None
     
@@ -38,24 +37,19 @@ class Contact:
         self.user_linkdln = linkdln
         self.user_adress = adress
 
-    def create_contact():
-        return None
     
-    def get_pers():
-        return None
-
-    def put_person():
-        return None
-    
-    def delete_person():
-        return None
-
-
-
 class Projects:
     def __init__(self, name, year, place, skills, only, status):
-        
+        self.project_name = name
+        self.project_year = year
+        self.project_place = place
+        self.project_skills = skills
+        self.project_only = only
+        self.project_status = status
+
+class testimonials:
+    
         
 
-
+# Surement que methode CRON elle sera gerer ailleurs 
 
