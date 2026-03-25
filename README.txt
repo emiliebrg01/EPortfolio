2b16254571be45217b6cddd6c273e9aea716a352
emiliebrg01 <emilie.bourg@epfedu.fr>
2026-03-25 12:37:15 +0100

    Création du fichier README avec les informations du projet et début du fichier main

---
+++ README.txt
@@ -0,0 +1,15 @@
+Maxence Baissas et Emilie Bourg travaillent sur un projet de Portfolio
+
+Objectif:
+Créer un site web sur lequel on peut générer un portfolio en entrant des données.
+
+But:
+Nous vendre à de potentiel recruteur. 
+D'une part sous forme d'un CV avec nos compétences et experiences.
+D'autre par en montrant ce qu'on sait faire à l'aide de ce site web.
+
+Comment:
+- Créer le frontend du site internet pour le visuel
+- Une base de données avec toutes les informations
+- Un backend avec toutes les fonctionnalités du code 
+
---
+++ main.py
@@ -0,0 +1,47 @@
+## Fichier main ##
+
+# Les classes à creer ! (chaque classe sera une partie du portfolio)
+
+# Classe personne (about me) : firstname, lastname, age, statut
+# Classe contact (mail, tel, linkdln, adress)
+#
+#
+# Classe expériences (name, year, place, skills, only (bool), )
+# Classe contact
+# Classe temoignage
+
+class Personne:
+    def __init__(self, firstname, lastname, age, statut)
+        self.user_firstname = firstname
+        self.user_lastname = lastname
+        self.user_age = age
+        self. statut = statut
+
+
+    def create_personne():
+    
+    def get_personne():
+
+    def put_personne():
+    
+    def delete_personne():
+    
+
+
+class Contact:
+    def __ini__(self, email, phone, linkdln, adress)
+        self.user_email = email
+        self.user_phone = phone
+        self.user_linkdln = linkdln
+        self.user_adress = adress
+
+    def Ajouter
+
+
+
+class Projects:
+    def __init__(self, )
+        
+
+
+
