8ca431131f5933f6f425f2efd30feab4645d5598
Maxence Baissas <m.baissas@orange.fr>
2026-04-01 16:45:49 +0200

    Ajout de commentaire

--- main.py
+++ main.py
@@ -1,58 +1,63 @@
-## Fichier main ##
+# ## Fichier main ##
 
-# Les classes à creer ! (chaque classe sera une partie du portfolio)
+# # Les classes à creer ! (chaque classe sera une partie du portfolio)
 
-# Classe personne (about me) : firstname, lastname, age, statut
-# Classe contact (mail, tel, linkdln, adress)
-#
-#
-# Classe expériences (name, year, place, skills, only (bool), status, )
-# Classe contact
-# Classe temoignage
-
-
-class Person:
-    def __init__(self, firstname, lastname, age, status):
-        self.user_firstname = firstname
-        self.user_lastname = lastname
-        self.user_age = age
-        self.statut = status
-
-    def create_person():
-        return None
-
-    def get_person():
-        return None
-
-    def put_person():
-        return None
-
-    def delete_person():
-        return None
+# # Classe personne (about me) : firstname, lastname, age, statut
+# # Classe contact (mail, tel, linkdln, adress)
+# #
+# #
+# # Classe expériences (name, year, place, skills, only (bool), status, )
+# # Classe contact
+# # Classe temoignage
 
 
-class Contact:
-    def __init__(self, email, phone, linkdln, adress):
-        self.user_email = email
-        self.user_phone = phone
-        self.user_linkdln = linkdln
-        self.user_adress = adress
+# class Person:
+#     def __init__(self, firstname, lastname, age, status):
+#         self.user_firstname = firstname
+#         self.user_lastname = lastname
+#         self.user_age = age
+#         self.statut = status
+
+#     def create_person():
+#         return None
+
+#     def get_person():
+#         return None
+
+#     def put_person():
+#         return None
+
+#     def delete_person():
+#         return None
 
 
-class Project:
-    def __init__(self, name, year, place, skills, only, status):
-        self.project_name = name
-        self.project_year = year
-        self.project_place = place
-        self.project_skills = skills
-        self.project_only = only
-        self.project_status = status
+# class Contact:
+#     def __init__(self, email, phone, linkdln, adress):
+#         self.user_email = email
+#         self.user_phone = phone
+#         self.user_linkdln = linkdln
+#         self.user_adress = adress
 
 
-class testimonials:
-    def __init__():
-        return None
+# class Project:
+#     def __init__(self, name, year, place, skills, only, status):
+#         self.project_name = name
+#         self.project_year = year
+#         self.project_place = place
+#         self.project_skills = skills
+#         self.project_only = only
+#         self.project_status = status
 
 
-# Surement que methode CRUB elle sera gerer ailleurs
-# Ne pas mettre self.project_name mais juste self.name car deja dans la classe
+# class testimonials:
+#     def __init__():
+#         return None
+
+
+# # Surement que methode CRUB elle sera gerer ailleurs
+# # Ne pas mettre self.project_name mais juste self.name car deja dans la classe
+
+
+
+
+
