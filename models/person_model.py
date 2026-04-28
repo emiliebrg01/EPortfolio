48d8a957c214b85437a03c84d7d3d970c5adfdd7
Maxence Baissas <m.baissas@orange.fr>
2026-04-28 16:56:26 +0200

    Ajout des tables sqlmodel dans person_model, il faurdra changer le nom person_model -> model

--- models/person_model.py
+++ models/person_model.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import date
 
 
 class Person (SQLModel, table=True):
@@ -8,3 +9,37 @@ class Person (SQLModel, table=True):
     lastname: str
     phone: str
     mail: str
+    linkedln : str
+
+
+class Experience (SQLModel, table=True):
+    id : Optional[int] = Field(default=None, primary_key=True)
+    job_name : str
+    company_name : str
+    date_start : date
+    date_end : date
+    type : str
+    description : str
+    id : int = Field(foreign_key="Person.id")
+
+
+class Skills (SQLModel, table=True):
+    id : Optional[int] = Field(default=None, primary_key=True)
+    name : str
+    level : int
+    id: int = Field(foreign_key="Person.id")
+
+class Formation (SQLModel, table=True):
+    id : Optional[int] = Field(default=None, primary_key=True)
+    name : str
+    date_start : date
+    date_end : date
+    secteur : str
+    type : str
+    person : int = Field(foreign_key="person.id")
+
+class Book (SQLModel, table=True):
+    id : Optional[int] = Field(default=None, primary_key=True)
+    photo : str
+    number : int 
+    person : int = Field(foreign_key="Person.id")
