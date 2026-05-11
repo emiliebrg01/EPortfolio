7d255f4c0284a2dfc0ee940c9eff94b231b6de56
Maxence Baissas <m.baissas@orange.fr>
2026-05-11 14:49:33 +0200

    renomage des fichiers repository controller service dto

--- schemas/dto.py
+++ schemas/dto.py
@@ -1,15 +1,16 @@
 from sqlmodel import SQLModel
 
+
 class PersonBaseDTO(SQLModel):
     firstname: str
     lastname: str
     phone: str
     mail: str
 
+
 class PersonDTO(PersonBaseDTO):
     pass
 
-class PersonResponseDTO(PersonBaseDTO):
-    id : int
 
-    
+class PersonResponseDTO(PersonBaseDTO):
+    id: int
