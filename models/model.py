0f98a15ff6cce9630862f424bcf9c7064c85ecba
Maxence Baissas <m.baissas@orange.fr>
2026-05-11 14:40:03 +0200

    remplacement nom du fichier de model et modification de la class Book

---
+++ models/model.py
@@ -0,0 +1,47 @@
+from datetime import date
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class Person(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    firstname: str
+    lastname: str
+    phone: str
+    mail: str
+    linkedin: str = ""
+
+
+class Experience(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    job_name: str
+    company_name: str
+    date_start: Optional[date] = None
+    date_end: Optional[date] = None
+    contract_type: str = ""
+    description: str = ""
+    person_id: int = Field(foreign_key="person.id")
+
+
+class Skills(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    level: int
+    person_id: int = Field(foreign_key="person.id")
+
+
+class Formation(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    date_start: Optional[date] = None
+    date_end: Optional[date] = None
+    secteur: str = ""
+    degree_type: str = ""
+    person_id: int = Field(foreign_key="person.id")
+
+
+class Book(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    photo: str
+    person_id: int = Field(foreign_key="person.id")
